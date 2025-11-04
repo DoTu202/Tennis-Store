dcd7b1364128c76e0b50120cb332521deedf8345
DoTu202 <mhtu1213@gmail.com>
2025-11-04 21:15:59 +0700

    banner

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import './Hero.css'
 
 
 function Hero() {
   return (
     <div className='hero'>
-
+        <div className='hero-overlay'>
+            <p>DISCOVER THE FUTURE</p>
+            <h1>Tennis Revolution:</h1>
+            <h2>Explore our selection<br/> and join the game today!</h2>
+            <button className='hero-btn'>EXPLORE</button>
+        </div>
     </div>
   )
 }
